d4aa7eb882c0f35fbdefba197888f84de97ea514
Victoria Martini <victoria.martini@etu.u-bordeaux.fr>
2026-02-06 16:27:25 +0100

    Ajout du test 29/02

--- module.test.js
+++ module.test.js
@@ -2,11 +2,6 @@ import { calculateAge } from "./module.js"
 
 
 describe('calculateAge Unit Test Suites', () => {
-        it('should return a correct age', () => {
-                const loise = { birth: new Date("02/03/1997") };
-                expect(calculateAge(loise)).toEqual(29)
-        })
-
         // Aucun argument n’a été envoyé
         it('should throw a "missing param p" error', () => {
                 expect(()=> calculateAge()).toThrow("missing param p")
@@ -41,14 +36,10 @@ describe('calculateAge Unit Test Suites', () => {
         it('should return a correct age', () => {
                 const today = new Date('2026-02-06').getTime();
                 jest.spyOn(Date, 'now').mockReturnValue(today);
-                const loise = { birth: new Date("02/03/1997") };
 
                 expect(calculateAge({ birth: new Date("02/03/1997") })).toEqual(29);
-                expect(calculateAge({ birth: new Date("06/22/1992") })).toEqual(33);
-                expect(calculateAge({ birth: new Date("03/22/2013") })).toEqual(12);
                 expect(calculateAge({ birth: new Date("10/21/2018") })).toEqual(7);
-
-
+                expect(calculateAge({ birth: new Date("02/29/2020") })).toEqual(5);
         })
 
 
