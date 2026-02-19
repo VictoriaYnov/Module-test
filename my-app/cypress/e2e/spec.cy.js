889a8776b5ba0a7163d45cdc33b53e32e3abca2d
Victoria Martini <victoria.martini@etu.u-bordeaux.fr>
2026-02-19 09:54:51 +0100

    Cypresse

---
+++ my-app/cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+
+
+describe('Home page spec', () => {
+   it('deployed react app to localhost', () => {
+      cy.visit('http://localhost:3000')
+      //cy.contains('1 user(s) already registered')
+   })
+})
