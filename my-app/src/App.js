d99ec55dcbc960e500ccd3e289348310a1b0c085
Victoria Martini <victoria.martini@etu.u-bordeaux.fr>
2026-02-19 14:22:00 +0100

    config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,7 +26,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home users={users} />} />
         <Route path="/register" element={<Register addUser={addUser} />} />
